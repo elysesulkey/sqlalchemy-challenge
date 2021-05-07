dddfec436ec9efff4ecddc62960e0e3c9efad6f2
Elyse Sulkey <elysesulkey@MacBook-Air.local>
2021-05-07 18:08:45 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+# Import Flask
+from flask import Flask, jsonify
+
 # Dependencies and Setup
 import numpy as np
+import pandas as pd
 import datetime as dt
 
 # Python SQL Toolkit and Object Relational Mapper
@@ -8,11 +12,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-# Import Flask
-from flask import Flask, jsonify
-
 # Database Setup
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Reflect Existing Database Into a New Model
 Base = automap_base()
@@ -20,10 +21,15 @@ Base = automap_base()
 # Reflect the Tables
 Base.prepare(engine, reflect=True)
 
+#view
+Base.classes.keys()
+
 # Save References to Each Table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create Session (Link) From Python to the DB
-#session = Session(engine)
+session = Session(engine)
 
+# Flask Setup
+app = Flask(__name__)
